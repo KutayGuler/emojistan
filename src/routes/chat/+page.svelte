b3ff5b6f77029af03b6e2c911aa4cb61d94057ae
KutayGuler <s7019@horyzont.eu>
2023-02-11 22:37:48 +0300

    shit there is going to be hell alot of edge cases

--- src/routes/chat/+page.svelte
+++ src/routes/chat/+page.svelte
@@ -1,29 +1,43 @@
 <script lang="ts">
-  import { fade, fly } from "svelte/transition";
+  import { onMount } from "svelte";
+  import { fly } from "svelte/transition";
+  import { notifications } from "../notifications";
 
   interface Choice {
     to: string;
     text: string;
+    consequences: Array<[string, number]>;
   }
 
   class Choice {
-    constructor(to: string, text: string) {
+    constructor(
+      to: string,
+      text: string,
+      consequences: Array<[string, number]>
+    ) {
       this.to = to;
       this.text = text;
+      this.consequences = consequences;
     }
   }
 
   let character = "🐸";
   let dialogueTree = new Map<string, Array<string | Array<Choice>>>();
   dialogueTree.set("1", [
-    "hello",
-    "what's up",
-    "n-word",
+    // "hello",
+    // "what's up",
+    // "n-word",
     "will you help me recover racism?",
-    [new Choice("2", "yes"), new Choice("3", "no")],
+    [
+      new Choice("1_1", "yes", [
+        ["🥇", -5],
+        ["🐐", 2],
+      ]),
+      new Choice("1_2", "no", [["🍆", 3]]),
+    ],
   ]);
 
-  dialogueTree.set("3", ["get ready to get destroyed babi"]);
+  dialogueTree.set("1_2", ["get ready to get destroyed babi"]);
 
   let currentKey = "1";
   let chatIndex = 0;
@@ -33,21 +47,78 @@
     chatIndex = 0;
     currentKey = to;
   }
+
+  onMount(() => {
+    chatIndex++;
+  });
+
+  let currentDialogue = dialogueTree.get(currentKey);
+  $: currentDialogue = dialogueTree.get(currentKey);
+
+  let currentText = "";
+
+  function addTextToDialogue(text: string) {
+    if (Array.isArray(currentDialogue?.at(-1))) {
+      notifications.warning("You cannot add text after choice");
+      return;
+    }
+
+    currentDialogue.push(text);
+  }
+
+  function addTextBeforeLast() {
+    // edge-case: can't have more than 1 array (choice) inside a dialogue
+  }
+
+  // TODO: Switch to dev branch before doing all that
+  // TODO: Add stack to Equippable | Consumable
+  // TODO: Consumables should also be pickable (green tint behind item if it is consumable)
+
+  function formatCost(cost: number) {
+    if (cost == 1) return "+";
+    if (cost == -1) return "-";
+
+    if (cost > 1) {
+      return "+" + cost;
+    }
+
+    return cost;
+  }
 </script>
 
 <svelte:window
   on:keydown={(e) => {
-    // if (animating) return;
     if (e.code == "Space") {
       chatIndex++;
+
+      if (Array.isArray((currentDialogue || [])[chatIndex])) {
+        chatIndex++;
+      }
     }
   }}
 />
 
-<!-- {#key currentKey} -->
-<h1 class="text-2xl">{character}</h1>
+<main class="p-4">
+  <input type="text" class="input input-bordered" bind:value={currentText} />
+  <button
+    class="btn"
+    on:click={() => {
+      addTextToDialogue(currentText);
+      currentText = "";
+    }}>add</button
+  >
+  <button
+    class="btn"
+    on:click={() => {
+      addTextToDialogue(currentText);
+      currentText = "";
+    }}>ADD CHOICE</button
+  >
+
+  <h1 class="text-2xl">{character}</h1>
+</main>
 <ul>
-  {#each dialogueTree.get(currentKey) || [] as dialogue, i}
+  {#each currentDialogue || [] as dialogue, i}
     {#if i < chatIndex && !animating}
       {#if Array.isArray(dialogue)}
         <div
@@ -60,9 +131,18 @@
           }}
         >
           {#each dialogue as choice}
-            <button class="btn w-1/6" on:click={() => makeChoice(choice.to)}
-              >{choice.text}</button
-            >
+            <button
+              class="btn flex h-24 w-1/6 flex-col py-4"
+              on:click={() => makeChoice(choice.to)}
+              ><p>{choice.text}</p>
+
+              <br />
+              <div>
+                {#each choice.consequences as [item, cost]}
+                  {formatCost(cost)} {item} <br />
+                {/each}
+              </div>
+            </button>
           {/each}
         </div>
       {:else}
@@ -75,24 +155,8 @@
     {/if}
   {/each}
 </ul>
-<!-- {/key} -->
 
-<!-- {#each [...dialogueTree] as [id, dialogue]}
-  <h1 class="text-2xl">{id}</h1>
-  {#each dialogue as d}
-    {#if Array.isArray(d)}
-      {#each d as choice}
-        <p>{choice.text}</p>
-        <p>{choice.to}</p>
-      {/each}
-    {:else}
-      <p>{d}</p>
-    {/if}
-  {/each}
-{/each} -->
 <style>
-  /* :not(:first-child) */
-
   li span {
     border-bottom-left-radius: 12px !important;
   }
@@ -100,7 +164,4 @@
   li span::before {
     display: none;
   }
-
-  /* .chat-bubble {
-  } */
 </style>
