e84a698efb5af3a026d7600158764012dae11e1d
KutayGuler <s7019@horyzont.eu>
2023-04-26 23:03:22 +0300

    xd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,39 +8,39 @@
 	import { currentSkin, loadedEmojis } from '$src/store';
 	import { emojis } from './editor/emojis';
 	import Loading from './Loading.svelte';
-	import { tweened } from 'svelte/motion';
-	import { cubicOut } from 'svelte/easing';
-	const progress = tweened(0, {
-		duration: 100,
-		easing: cubicOut,
-	});
+	// import { tweened } from 'svelte/motion';
+	// import { cubicOut } from 'svelte/easing';
+	// const progress = tweened(0, {
+	// 	duration: 100,
+	// 	easing: cubicOut,
+	// });
 
 	// supabase.auth.onAuthStateChange((event, session) => {});
 
-	const TOTAL_SVG_COUNT = 1817;
-	let svgCount = 0;
+	// const TOTAL_SVG_COUNT = 1817;
+	// let svgCount = 0;
 
-	function waitForEmojisLoad(node: Element) {
-		if ($loadedEmojis) return;
-		for (let child of Array.from(node.children)) {
-			const img = new Image();
-			img.src = getComputedStyle(child).backgroundImage.replace(
-				/(^url\()|(\)$|[\"\'])/g,
-				''
-			);
+	// function waitForEmojisLoad(node: Element) {
+	// 	if ($loadedEmojis) return;
+	// 	for (let child of Array.from(node.children)) {
+	// 		const img = new Image();
+	// 		img.src = getComputedStyle(child).backgroundImage.replace(
+	// 			/(^url\()|(\)$|[\"\'])/g,
+	// 			''
+	// 		);
 
-			img.addEventListener('load', () => {
-				svgCount += 1;
-				progress.set(svgCount / TOTAL_SVG_COUNT);
-				if (svgCount >= TOTAL_SVG_COUNT) {
-					$loadedEmojis = true;
-				}
-			});
-		}
-	}
+	// 		img.addEventListener('load', () => {
+	// 			svgCount += 1;
+	// 			progress.set(svgCount / TOTAL_SVG_COUNT);
+	// 			if (svgCount >= TOTAL_SVG_COUNT) {
+	// 				$loadedEmojis = true;
+	// 			}
+	// 		});
+	// 	}
+	// }
 </script>
 
-<div class="hidden">
+<!-- <div class="hidden">
 	{#each Object.keys(emojis) as category}
 		<div use:waitForEmojisLoad>
 			{#each emojis[category] as name}
@@ -51,14 +51,10 @@
 			{/each}
 		</div>
 	{/each}
-</div>
+</div> -->
 
-{#if !$loadedEmojis || ($navigating && !$navigating.from?.route.id?.includes('tut'))}
+{#if $navigating && !$navigating.from?.route.id?.includes('tut')}
 	<Loading />
-	<progress
-		class="progress progress-primary absolute bottom-0 z-20 w-full"
-		value={$progress}
-	/>
 {/if}
 
 <Toast />
