ccba60cdd10ef81e2c543d40a234c18f57d2a97a
KutayGuler <s7019@horyzont.eu>
2023-05-09 12:53:44 +0300

    removed preloading svgs with img elements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,51 +24,7 @@
 
 		return () => subscription.unsubscribe();
 	});
-
-	// import { tweened } from 'svelte/motion';
-	// import { cubicOut } from 'svelte/easing';
-	// const progress = tweened(0, {
-	// 	duration: 100,
-	// 	easing: cubicOut,
-	// });
-
-	// supabase.auth.onAuthStateChange((event, session) => {});
-
-	// const TOTAL_SVG_COUNT = 1817;
-	// let svgCount = 0;
-
-	// function waitForEmojisLoad(node: Element) {
-	// 	if ($loadedEmojis) return;
-	// 	for (let child of Array.from(node.children)) {
-	// 		const img = new Image();
-	// 		img.src = getComputedStyle(child).backgroundImage.replace(
-	// 			/(^url\()|(\)$|[\"\'])/g,
-	// 			''
-	// 		);
-
-	// 		img.addEventListener('load', () => {
-	// 			svgCount += 1;
-	// 			progress.set(svgCount / TOTAL_SVG_COUNT);
-	// 			if (svgCount >= TOTAL_SVG_COUNT) {
-	// 				$loadedEmojis = true;
-	// 			}
-	// 		});
-	// 	}
-	// }
 </script>
-
-<!-- <div class="hidden">
-	{#each Object.keys(emojis) as category}
-		<div use:waitForEmojisLoad>
-			{#each emojis[category] as name}
-				<i
-					on:load={() => {}}
-					class="twa twa-{name.replace('_', $currentSkin)}"
-				/>
-			{/each}
-		</div>
-	{/each}
-</div> -->
 
 {#if $navigating && !$navigating.from?.route.id?.includes('tut')}
 	<Loading />
