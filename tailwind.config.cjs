abde3d600b6276b1b66ced090cf4a056ecde207e
KutayGuler <s7019@horyzont.eu>
2023-08-11 15:36:09 +0300

    removed unnecessary dependencies

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,7 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  plugins: [require("daisyui")],
   daisyui: {
     themes: ["emerald"],
   },
